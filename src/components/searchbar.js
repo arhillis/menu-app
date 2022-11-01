e07278efe065e20458968e8411f111483bb68688
Andrielle Hillis <andriellerhillis@gmail.com>
2022-10-31 19:57:52 -0500

    Basic coding for serchbar

--- src/components/searchbar.js
+++ src/components/searchbar.js
@@ -1,7 +1,11 @@
 function SearchBar(){
-    return (<>
-        Searchbar goes here...
-    </>)
+    return (<div className="search-container">
+        <form>
+            <input type="text" className="form-input" placeholder="type favorite meal"/>
+            <button className="btn">Search</button>
+            <button className="btn btn-hipster">Suprise me!</button>
+        </form>
+    </div>)
 }
 
 export default SearchBar;
