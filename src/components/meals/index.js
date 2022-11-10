267823c92a19584e939ff37a53b8774681c7b365
Andrielle Hillis <andriellerhillis@gmail.com>
2022-11-10 08:57:42 -0600

    Meal component added

--- src/components/meals/index.js
+++ src/components/meals/index.js
@@ -1,9 +1,12 @@
 import { useMenuContext } from '../../Context';
+import Meal from './meal';
 
 function Meals(){
     const {meals} = useMenuContext();
-    if(meals) console.log('from meals component', meals);
-    return (<section className='section section-center'>Meals go here...</section>)
+    if(meals) console.log('from meals component', meals[0]);
+    return (<section className='section section-center'>
+            {meals.map((meal, index) => (<Meal key={index} meal={meal}/>))}
+    </section>)
 }
 
 export default Meals;
