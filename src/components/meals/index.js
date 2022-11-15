7a8e2232e908d7238b61ae1a66c4068103b54ebe
Andrielle Hillis <andriellerhillis@gmail.com>
2022-11-15 10:39:46 -0600

    Loading prop added

--- src/components/meals/index.js
+++ src/components/meals/index.js
@@ -2,7 +2,12 @@ import { useMenuContext } from '../../Context';
 import Meal from './meal';
 
 function Meals(){
-    const {meals} = useMenuContext();
+    const {meals, loading} = useMenuContext();
+
+    if(loading)
+        return (<section className='section section-center'>
+                    <h4>Loading meals...</h4>
+                </section>)
     if(meals) console.log('from meals component', meals[0]);
     return (<section className='section section-center'>
             {meals.map((meal, index) => (<Meal key={index} meal={meal}/>))}
