3205c42097ede2100aa8ec4ceca8c2d5eaf851b1
Andrielle Hillis <andriellerhillis@gmail.com>
2022-11-15 10:49:47 -0600

    Added a view for when there are no meals

--- src/components/meals/index.js
+++ src/components/meals/index.js
@@ -8,10 +8,18 @@ function Meals(){
         return (<section className='section section-center'>
                     <h4>Loading meals...</h4>
                 </section>)
-    if(meals) console.log('from meals component', meals[0]);
+    if(meals) {
+        console.log('from meals component', meals[0]);
+        return (<section className='section section-center'>
+                        {meals.map((meal, index) => 
+                            (<Meal key={index} meal={meal}/>)
+                        )}
+                </section>)
+    }
+
     return (<section className='section section-center'>
-            {meals.map((meal, index) => (<Meal key={index} meal={meal}/>))}
-    </section>)
+                    <h4>No meal to show...</h4>
+                </section>)
 }
 
 export default Meals;
