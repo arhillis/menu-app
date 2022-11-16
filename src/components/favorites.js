81a9cf2624edbe07d89927b4ac7392c5e84aa3ad
Andrielle Hillis <andriellerhillis@gmail.com>
2022-11-16 07:54:38 -0600

    Basic favorites outline

--- src/components/favorites.js
+++ src/components/favorites.js
@@ -1,7 +1,14 @@
 function Favorites(){
-    return (<>
-        Favorites go here...
-    </>)
+    const favorites = localStorage.getItem('favorites');
+    console.log(favorites);
+    
+    return (<div className="favorites">
+                <div className="favorites-content">
+                    <div className="favorites-container">
+                        <h5>Favorites</h5>
+                    </div>                    
+                </div>                
+            </div>)
 }
 
 export default Favorites
