17c490505bcb7dbe53e992c1bbe5dc0f1814b749
Andrielle Hillis <andriellerhillis@gmail.com>
2022-11-17 15:18:03 -0600

    Modal content

--- src/components/modal.js
+++ src/components/modal.js
@@ -1,5 +1,27 @@
+import {useMenuContext} from '../Context';//
+import { AiFillYoutube } from "react-icons/ai";
+
 function Modal(){
-    return (<>Modal goes here...</>)
+    const {selectedMeal, hideModal} = useMenuContext();
+
+    if(!selectedMeal) return null;
+
+    return (<div className="modal-overlay">
+                <div className="modal-container">
+                    <img src={selectedMeal.strMealThumb} className='modal-img' alt={selectedMeal.strMeal}/>
+                    <div className='modal-content'>
+                        <h3>{selectedMeal.strMeal}</h3>
+                        <p>
+                            {selectedMeal.strInstructions}
+                            <a href={selectedMeal.strYoutube} target='_new'>
+                                Watch on &nbsp;
+                                <AiFillYoutube size='2rem'/>
+                            </a>
+                        </p>
+                        <button className=' btn close-btn' onClick={hideModal}>Close</button>
+                    </div>
+                </div>                
+            </div>)
 }
 
 export default Modal
