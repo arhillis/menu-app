39fd3e05fd9578f60fa62c16098ee0b1b0c58769
Andrielle Hillis <andriellerhillis@gmail.com>
2022-11-19 11:21:07 -0600

    Removed console.log, beautified buttons

--- src/components/meals/meal.js
+++ src/components/meals/meal.js
@@ -1,15 +1,21 @@
 import { BsFillHeartFill } from "react-icons/bs";
 import {useMenuContext} from '../../Context';
 function Meal({meal}){
-    const {selectMeal} = useMenuContext();
+    const {selectMeal, saveFavorites} = useMenuContext();
 
     return (<section className='single-meal'>
         <img src={meal.strMealThumb} alt={meal.strMeal}/>
         <footer>
             <h5>{meal.strMeal}</h5>
             <div className="btn-row">
-                <button className="btn view-btn" onClick={() => selectMeal(meal.idMeal)}>Details</button>
-                <button className="like-btn"><BsFillHeartFill /></button>
+                <button className="btn view-btn" 
+                    onClick={() => selectMeal(meal.idMeal)}>
+                        Details
+                </button>
+                <button className="like-btn" 
+                    onClick={() => saveFavorites(meal)}>
+                        <BsFillHeartFill />
+                </button>
             </div>
             
         </footer>
