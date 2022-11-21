9f753bce14c51ef6471ca1b8a6709b8644b59015
Andrielle Hillis <andriellerhillis@gmail.com>
2022-11-21 08:31:36 -0600

    Showing each favorite meal

--- src/components/favorites.js
+++ src/components/favorites.js
@@ -1,14 +1,28 @@
 import {useMenuContext} from '../Context';
 
 function Favorites(){    
-    const {favorites} = useMenuContext();
+    const {favorites, removeFromFavorites, selectMeal} = useMenuContext();
 
     return (<div className="favorites">
                 <div className="favorites-content">
                     <h5>Favorites</h5>
                     <div className="favorites-container">
                         {favorites.length > 0 ? 
-                            (<>Favorites go here...</>) :
+                            favorites.map(meal =>
+                            (<div className='favorite-item' 
+                                key={meal.idMeal}>
+                                    <img 
+                                        className='img favorites-img'
+                                        src={meal.strMealThumb} 
+                                        alt={meal.strMeal}
+                                        onClick={() => selectMeal(meal.idMeal)}
+                                    />
+                                    <button className='remove-btn'
+                                        onClick={() => removeFromFavorites(meal.idMeal)}
+                                    >
+                                        remove
+                                    </button>
+                            </div>)) :
                             (<>No favorites to show...</>)
                         }
                     </div>                    
